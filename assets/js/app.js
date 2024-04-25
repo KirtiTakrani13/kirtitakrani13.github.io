0600a08c18dba22e798740ce9a122a48a83ec076
KirtiTakrani13 <kirtitakrani0988@gmail.com>
2024-04-24 23:09:35 -0400

    Research Done
    Worked

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,9 @@ document.body.appendChild(script);
 
 //    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
 // Header
+{
+  /* <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li> */
+}
 
 let header = $(`
 <nav class="navbar navbar-expand-lg fixed-top dark-theme" id="navbar">
@@ -26,7 +29,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
+
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/kirti_Takrani_Resume_new.pdf" download>Download CV</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/cover_Letter_k.pdf" download>Cover Letter</a></li>
