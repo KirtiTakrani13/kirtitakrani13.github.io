e32b914551c1749fba8c07442f4877493271053c
KirtiTakrani13 <kirtitakrani0988@gmail.com>
2024-04-24 23:12:45 -0400

    push code
    push code

--- assets/js/sem_temp.js
+++ assets/js/sem_temp.js
@@ -2,7 +2,9 @@
 
 /* Common Page Content */
 // Header
-
+{
+  /* <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li> */
+}
 //    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
 let header = $(`
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="navbar">
@@ -22,7 +24,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
+  
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/kirti_Takrani_Resume_new.pdf" download>Download CV</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/cover_Letter_k.pdf.pdf" download>Cover Letter</a></li>
